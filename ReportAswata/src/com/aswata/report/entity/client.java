e203d735f79bb01fa1b7d0077c9758bc3b258ba5
Tri Wahyudi <Tri Wahyudi@tri-pc>
2020-02-20 09:23:52 +0700

    UPDATE

---
+++ ReportAswata/src/com/aswata/report/entity/client.java
@@ -0,0 +1,37 @@
+package com.aswata.report.entity;
+
+public class client {
+	
+	private String clientId;
+	private String name;
+	private String type;
+	private String parentId;
+	public String getClientId() {
+		return clientId;
+	}
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getParentId() {
+		return parentId;
+	}
+	public void setParentId(String parentId) {
+		this.parentId = parentId;
+	}
+	
+	
+	
+	
+}
---
+++ ReportAswata/src/com/aswata/report/entity/regLks.java
@@ -0,0 +1,128 @@
+package com.aswata.report.entity;
+
+import java.math.BigDecimal;
+
+public class regLks {
+
+	private String uwYear;
+	private String branch;
+	private String bsn;
+	private String reqId;
+	private String reqName;
+	private String lksNo;
+	private String lksDate;
+	private String lossDate;
+	private String policyNo;
+	private String period;
+	private String insuredName;
+	private String objectName;
+	private String tsi;
+	private BigDecimal lossEstimation;
+	private String type;
+	private String siCcy;
+	private String estCcy;
+	public String getUwYear() {
+		return uwYear;
+	}
+	public void setUwYear(String uwYear) {
+		this.uwYear = uwYear;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public String getBsn() {
+		return bsn;
+	}
+	public void setBsn(String bsn) {
+		this.bsn = bsn;
+	}
+	public String getReqId() {
+		return reqId;
+	}
+	public void setReqId(String reqId) {
+		this.reqId = reqId;
+	}
+	public String getReqName() {
+		return reqName;
+	}
+	public void setReqName(String reqName) {
+		this.reqName = reqName;
+	}
+	public String getLksNo() {
+		return lksNo;
+	}
+	public void setLksNo(String lksNo) {
+		this.lksNo = lksNo;
+	}
+	public String getLksDate() {
+		return lksDate;
+	}
+	public void setLksDate(String lksDate) {
+		this.lksDate = lksDate;
+	}
+	public String getLossDate() {
+		return lossDate;
+	}
+	public void setLossDate(String lossDate) {
+		this.lossDate = lossDate;
+	}
+	public String getPolicyNo() {
+		return policyNo;
+	}
+	public void setPolicyNo(String policyNo) {
+		this.policyNo = policyNo;
+	}
+	public String getPeriod() {
+		return period;
+	}
+	public void setPeriod(String period) {
+		this.period = period;
+	}
+	public String getInsuredName() {
+		return insuredName;
+	}
+	public void setInsuredName(String insuredName) {
+		this.insuredName = insuredName;
+	}
+	public String getObjectName() {
+		return objectName;
+	}
+	public void setObjectName(String objectName) {
+		this.objectName = objectName;
+	}
+	public String getTsi() {
+		return tsi;
+	}
+	public void setTsi(String tsi) {
+		this.tsi = tsi;
+	}
+	public BigDecimal getLossEstimation() {
+		return lossEstimation;
+	}
+	public void setLossEstimation(BigDecimal lossEstimation) {
+		this.lossEstimation = lossEstimation;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getSiCcy() {
+		return siCcy;
+	}
+	public void setSiCcy(String siCcy) {
+		this.siCcy = siCcy;
+	}
+	public String getEstCcy() {
+		return estCcy;
+	}
+	public void setEstCcy(String estCcy) {
+		this.estCcy = estCcy;
+	}
+	
+	
+}
